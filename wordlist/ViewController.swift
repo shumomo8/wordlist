21744cf1e1b2771da856b897aba1d5062e53d3ae
shu fujita <momo.8jack@gmail.com>
2020-05-23 15:52:45 +0900

    Initial Commit

---
+++ wordlist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wordlist
+//
+//  Created by Shu Fujita on 2020/05/23.
+//  Copyright © 2020 Fujita shu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
